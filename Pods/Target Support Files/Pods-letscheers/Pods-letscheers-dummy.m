ca4338afd27f4873e8c1b90d35f3506b79623eda
leesam <toyboy2@hanmail.net>
2021-11-21 11:50:36 +0900

    Removes Pods/

--- Pods/Target Support Files/Pods-letscheers/Pods-letscheers-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_letscheers : NSObject
-@end
-@implementation PodsDummy_Pods_letscheers
-@end
--- Pods/Target Support Files/Pods-letscheersUITests/Pods-letscheersUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_letscheersUITests : NSObject
-@end
-@implementation PodsDummy_Pods_letscheersUITests
-@end
